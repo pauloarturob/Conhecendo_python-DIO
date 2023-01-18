ed10a353275b07f328c8b84ad259640be8b4c345
Paulo <pauloarturob@gmail.com>
2023-01-18 19:54:08 -0300

    Atualização sistema bancario

--- testes.py
+++ testes.py
@@ -1,19 +1,3 @@
-lista = [1, 2, 4, 66, 78]
-print(lista)
-
-lista.append(int(input('Insira o valor desejado: ')))
-
-lista.sort()
-print(lista)
-lista.extend(int(input('Digite mais: ')))
-
-remover = (int(input('Digite o numero a ser removido: ')))
-
-if remover in lista:
-    lista.remove(remover)
-    print(f'O numero {remover} foi encotrado e deletado\n'
-          f'veja a a lista: {lista}')
-else:
-    print(f'numero {remover} não foi encontrado!')
-
-print(f'Contem {len(lista)} numeros na lista')
+C = int(input()) 
+for i in range (C):
+    print("Mais de 8000!" if int(input()) > 8000 else "Inseto!")
